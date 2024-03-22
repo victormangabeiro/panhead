bafd09f99f767b2600e59115422db12c620f621b
Victor <victorgabryel@hotmail.com>
2024-03-21 22:05:12 -0300

    feat: dark mode

--- app.vue
+++ app.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
-const nav = [
-  {
-    label: 'Home',
-    to: '/',
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
   },
-  {
-    label: 'External',
-    to: '/external',
-  },
-  {
-    label: 'Component',
-    to: '/component',
-  },
-]
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
 </script>
 
 <template>
   <div class="w-full h-full">
-    <header class="w-full h-full bg-gray-200 flex items-center pl-8">
+    <header class="w-full h-full flex items-center pl-8">
       <h1 class="text-2xl font-bold">PWA</h1>
+      <ClientOnly>
+        <UButton
+          :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+          color="gray"
+          variant="ghost"
+          aria-label="Theme"
+          @click="isDark = !isDark"
+        />
+        <template #fallback>
+          <div class="w-8 h-8" />
+        </template>
+      </ClientOnly>
     </header>
     <main class="w-full h-full pt-4 px-8">
   
